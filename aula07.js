ac8962a155b1b476423fb57e377ee12afb19f9fa
Fabio S. Silva <fabio_silva76@outlook.com>
2021-11-27 22:08:13 -0300

    Praticando Exercícios Js

---
+++ aula07.js
@@ -0,0 +1,6 @@
+var vel= 15
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60 ){//condição simples
+    console.log(` Ultrapassou a velocidade permitida. Você foi Multado`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
