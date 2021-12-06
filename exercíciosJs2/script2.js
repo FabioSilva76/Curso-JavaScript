9757ed952c9dbb4184756a67682fbd3ade36f086
Fabio S. Silva <fabio_silva76@outlook.com>
2021-12-06 14:07:22 -0300

    exercícios

--- exercíciosJs2/script2.js
+++ exercíciosJs2/script2.js
@@ -1,3 +1,44 @@
 function verificar() {
-    window.alert ('FUNCIONOU!')
+    var data= new Date()
+    var ano= data.getFullYear()
+    var fano= document.getElementById('txtano')
+    var res= document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano ) {
+        window.alert('[Erro] Verifique os dados e tente novamente!')
+    }else{
+       var fsex= document.getElementsByName('radsex')
+       var idade= ano - Number(fano.value)
+       var genero= ''
+       var img= document.createElement('img')
+       img.setAttribute('id', 'foto')
+
+       if (fsex[0].checked) {
+           genero= 'Homen'
+           if (idade >= 0 && idade < 10){
+               // criança
+           }else if (idade < 21) {
+               // Jovem
+           }else if (idade < 50) {
+               // Adulto
+           }else {
+               //Idoso
+           }
+
+       }else if (fsex[1].checked) {
+           genero= 'Mulher'
+           if (idade >= 0 && idade < 10){
+            // criança
+        }else if (idade < 21) {
+            // Jovem
+        }else if (idade < 50) {
+            // Adulto
+        }else {
+            //Idoso
+        }
+       }
+       res.style.textAlign= 'center'
+       res.innerHTML= `Detectamos ${genero} com ${idade} anos.`
+       
+    }
+
 }
